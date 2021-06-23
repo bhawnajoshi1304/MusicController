d8ba98079088b635e417044198c0aad32187f123
Bhawna Joshi <bhawnajoshi1304@gmail.com>
2021-06-24 04:30:41 +0530

    api create room created

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,7 @@ class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model=Room
         fields='__all__'
-        
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Room
+        fields=('guest_can_pause','votes_to_skip') 
