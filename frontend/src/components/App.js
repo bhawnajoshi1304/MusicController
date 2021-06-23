c15facf92030908188c1bda1b790efd84a71b501
Bhawna Joshi <bhawnajoshi1304@gmail.com>
2021-06-24 03:12:16 +0530

    Added more pages

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+import CreateRoomPage from "./CreateRoomPage";
+import RoomJoinPage from "./RoomJoinPage";
 
 export default class App extends Component {
   constructor(props) {
@@ -7,7 +10,13 @@ export default class App extends Component {
   }
 
   render() {
-    return (<h1>Testing React Code</h1>    );
+    return (
+      <div>
+        <HomePage />
+        <CreateRoomPage/>
+        <RoomJoinPage/>
+      </div>
+    );
   }
 }
 
