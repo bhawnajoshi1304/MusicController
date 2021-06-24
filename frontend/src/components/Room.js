4ec35de00f0c99b7822bbfa5279228155208a8ad
Bhawna Joshi <bhawnajoshi1304@gmail.com>
2021-06-25 03:26:38 +0530

    create room page now redirects to room page

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -5,10 +5,23 @@ export default class Room extends Component {
         super(props);
         this.state = {
             votesToSkip: 2,
-            guesstCanPause: false,
+            guestCanPause: false,
             isHost: false,
         };
         this.roomCode = this.props.match.params.roomCode;
+        this.getRoomDetails();
+    }
+
+    getRoomDetails() {
+        fetch('/api/get-room' + "?code=" + this.roomCode)
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    votesToSkip: data.votes_to_skip,
+                    guestCanPause: data.guest_can_pause,
+                    isHost: data.isHost,
+                });
+            });
     }
 
     render() {
@@ -16,8 +29,8 @@ export default class Room extends Component {
             <div>
                 <h3>{this.roomCode}</h3>
                 <p>Votes: {this.state.votesToSkip}</p>
-                <p>Guest Can Pause: {this.state.guesstCanPause}</p>
-                <p>Host: {this.state.isHost}</p>
+                <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
+                <p>Host: {this.state.isHost.toString()}</p>
             </div>
         );
     }
