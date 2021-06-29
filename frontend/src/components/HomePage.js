ef8c822c2901c1dc7d7bd815df716ea8eae08346
Bhawna Joshi <bhawnajoshi1304@gmail.com>
2021-06-30 04:49:49 +0530

    django sessions and leave room

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -17,6 +17,7 @@ export default class HomePage extends Component {
     this.state = {
       roomCode: null,
     };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
   }
 
   async componentDidMount() {
@@ -51,6 +52,12 @@ export default class HomePage extends Component {
     );
   }
 
+  clearRoomCode() {
+    this.setState({
+      roomcode: null,
+    });
+  }
+
   render() {
     return (
       <Router>
@@ -63,7 +70,12 @@ export default class HomePage extends Component {
           />
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
-          <Route path="/room/:roomCode" component={Room} />
+          <Route
+            path="/room/:roomCode"
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+            }}
+          />
         </Switch>
       </Router>
     );
