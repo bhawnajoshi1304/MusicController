b1aa9f5f26c0dfa0d11a6746b3cca8f5e416ed3f
Bhawna Joshi <bhawnajoshi1304@gmail.com>
2021-08-26 16:03:06 +0530

    Skip song functionality added

--- frontend/src/components/MusicPlayer.js
+++ frontend/src/components/MusicPlayer.js
@@ -9,6 +9,14 @@ export default class MusicPlayer extends Component {
     super(props);
   }
 
+  skipSong() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("/spotify/skip", requestOptions);
+  }
+
   pauseSong() {
     const requestOptions = {
       method: "PUT",
@@ -42,14 +50,15 @@ export default class MusicPlayer extends Component {
               {this.props.artist}
             </Typography>
             <div>
-            <IconButton
+              <IconButton
                 onClick={() => {
                   this.props.is_playing ? this.pauseSong() : this.playSong();
                 }}
               >
                 {this.props.is_playing ? <PauseIcon /> : <PlayArrowIcon />}
               </IconButton>
-              <IconButton>
+              <IconButton onClick={() => this.skipSong()}>
+                {this.props.votes} / {this.props.votes_required}
                 <SkipNextIcon />
               </IconButton>
             </div>
